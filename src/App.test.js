b033882944769b5e317af4d405e18804c7a5d3de
Dan Hass <hass.dan@gmail.com>
2023-11-13 10:45:10 -0600

    Eliminated default React info

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Dantech Finance title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/DanTech Finance/i);
   expect(linkElement).toBeInTheDocument();
 });
